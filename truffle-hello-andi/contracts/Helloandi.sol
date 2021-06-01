e908b1b0786db8df892eb6a24106eb21cb30a509
anditech <71329092+anditech@users.noreply.github.com>
2021-05-31 22:08:53 -0400

    Update Excercises
    Testing error handling and async functions

--- truffle-hello-andi/contracts/Helloandi.sol
+++ truffle-hello-andi/contracts/Helloandi.sol
@@ -1,13 +1,12 @@
 pragma solidity 0.5.12;
 
 contract Helloandi {
-    string message = "Hello Andi!";
+    string message = "Hello Pucon!";
 
     function getMessage() public view returns (string memory) {
         return message;
     }
     function setMessage(string memory newMessage) public payable {
-        require(msg.value >= 1 ether);
         message = newMessage;
     }
 }
--- truffle-hello-andi/migrations/2_Helloandi_deploy.js
+++ truffle-hello-andi/migrations/2_Helloandi_deploy.js
@@ -1,3 +1,45 @@
+const Helloandi = artifacts.require('Helloandi');
+
+module.exports = (deployer, network, accounts) => {    // function(deployer, network, accounts) {
+    deployer.deploy(Helloandi).then(instance => {     // .then(function(instance) {
+        instance.setMessage('Hello Ruca-Pillan!').then(() => {   // .then(function() {
+            instance.getMessage().then(message => {        // .then(function(message) {
+                console.log(`Current Message: ${message}`);   // ('Current Message: ' + message);
+            }).catch(err => console.log(` **GETMESSAGE** ${err}`));
+        }).catch(err => console.log(` **SETMESSAGE** ${err}`));
+        // .catch(function(err) {
+        //    console.log(` **SET/GETMESSAGE** ${err}`);
+        //  });
+    });
+};
+
+
+
+
+
+/*
+
+
+
+
+
+const Helloandi = artifacts.require("Helloandi");
+
+module.exports = async function(deployer, network, accounts) {
+                  // async (deployer, network, accounts) => {
+    try {
+        await deployer.deploy(Helloandi);
+        let instance = await Helloandi.deployed();
+        await instance.setMessage("Hello EL SUL!");
+        let message = await instance.getMessage();
+        console.log(`Current Message: ${message}`);
+    }
+    catch (err) {
+        console.log(`*You've got an error* ${err}`);
+    }
+};
+
+
 const Helloandi = artifacts.require("Helloandi");
 
 module.exports = function (deployer, network, accounts) {
@@ -9,3 +51,10 @@ module.exports = function (deployer, network, accounts) {
         });
     });
 };
+
+
+
+
+
+
+*/
